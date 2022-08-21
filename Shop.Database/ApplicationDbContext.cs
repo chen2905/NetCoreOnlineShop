f7e7bda4f1642ca13b269b766d2e5911be85d00f
Max <maxlin2905@gmail.com>
2022-08-21 20:12:34 +1000

    episode 3 projects setup

--- Shop.Database/ApplicationDbContext.cs
+++ Shop.Database/ApplicationDbContext.cs
@@ -11,7 +11,7 @@ namespace Shop.Database
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base (options)
             {
-
+               
             }
 
         }
