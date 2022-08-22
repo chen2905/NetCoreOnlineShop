d9b4b446f9f795f7bf67d3d53ce02c20c9b2911f
Max <maxlin2905@gmail.com>
2022-08-22 10:51:04 +1000

    episode 4 first database migrations with EF

--- Shop.UI/Startup.cs
+++ Shop.UI/Startup.cs
@@ -1,13 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Shop.Database;
 
 namespace Shop.UI
     {
@@ -15,14 +12,17 @@ namespace Shop.UI
         {
         public Startup(IConfiguration configuration)
             {
-            Configuration = configuration;
+            _config = configuration;
             }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration _config { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
             {
+
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(_config["DefaultConnection"]));
             services.AddRazorPages();
             }
 
