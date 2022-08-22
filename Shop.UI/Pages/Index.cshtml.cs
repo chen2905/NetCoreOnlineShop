9f4b70e9b84fdce3554126072f6e6c8d1fb16585
Max <maxlin2905@gmail.com>
2022-08-22 12:42:50 +1000

    episode 5 first part

--- Shop.UI/Pages/Index.cshtml.cs
+++ Shop.UI/Pages/Index.cshtml.cs
@@ -1,15 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Shop.UI.Pages
     {
     public class IndexModel : PageModel
         {
+
+        public class ProductViewModel
+            {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Value { get; set; }
+            }
+
+        [BindProperty]//BindProperty says Product is the main model of the view
+        public ProductViewModel Product { get; set; }
+
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
