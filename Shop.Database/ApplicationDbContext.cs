3fcd4c60e8aaac2c5a03dc1345b87332cbfab18a
Max <maxlin2905@gmail.com>
2022-08-23 10:31:48 +1000

    episode 8 create many to many models and let EF map to database

--- Shop.Database/ApplicationDbContext.cs
+++ Shop.Database/ApplicationDbContext.cs
@@ -16,5 +16,19 @@ namespace Shop.Database
                
             }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Stock> Stock { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+            {
+            base.OnModelCreating(builder);
+
+            builder.Entity<OrderProduct>()
+                .HasKey(x => new { x.ProductId, x.OrderId });
+            //creating a primary key based on Product and Order ids
+            }
         }
     }
